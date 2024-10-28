e6d0cf2afe49db73693804519fd750dbda531369
pedrohenriquevaz <pedro.andrade@siscobra.com.br>
2024-10-27 22:06:53 -0300

    add inicial game page

--- js/script.js
+++ js/script.js
@@ -2,11 +2,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const actor = document.querySelector('.actor');
     const icon = document.querySelector('.icon');
     const images = ["fench.png", "js.png", "java.png", "php.png", , "postgresql.png"];
+    const gameBoard = document.querySelector('.game-board');
+    const startScreen = document.querySelector('.start-screen');
+    const startButton = document.getElementById('start-button');
+    const characterOptions = document.querySelectorAll('.character-option');
+    let selectedCharacter = './images/actor.gif';
+
+    characterOptions.forEach(option => {
+        option.addEventListener('click', (event) => {
+            selectedCharacter = event.target.getAttribute('data-character');
+
+/*             characterOptions.forEach(opt => {
+                opt.classList.remove('selected');
+            }); */
     
+            event.target.classList.add('selected');
+        });
+    });
+    
+    function startGame() {
+        actor.src = selectedCharacter; 
+        startScreen.style.display = 'none'; 
+        gameBoard.style.display = 'block'; 
+    }
+
     function changeiconImage() {
         const randomIndex = Math.floor(Math.random() * images.length);
         icon.src = `./images/${images[randomIndex]}`;
     }
+
+    startButton.addEventListener('click', startGame);
 
     actor.addEventListener('animationend', () => {
         actor.classList.remove('jump');
@@ -18,21 +43,23 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Intervalo para verificar o estado do jogo e a troca de imagem da icon
+
     const gameOver = setInterval(() => {
         const iconPosition = icon.offsetLeft;
         const actorPosition = Number(window.getComputedStyle(actor).bottom.replace('px', ''));
 
         if (iconPosition <= 120 && iconPosition > 0 && actorPosition < 70) {
+
             icon.style.animation = 'none';
             icon.style.left = `${iconPosition}px`;
+
             actor.style.animation = 'none';
             actor.style.bottom = `${actorPosition}px`;
 
             clearInterval(gameOver);
         } 
         
-        if (iconPosition <= -10) {
+        if (iconPosition < 0) {
             changeiconImage();
         }
 
